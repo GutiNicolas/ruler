422413fda8bada69149edabfaf3f2f7da301c3e1
GutiNicolas <luis.gutierrez@despegar.com>
2020-05-19 21:28:53 -0300

    First commit

---
+++ src/test/java/com/github/gutinicolas/ruler/RulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gutinicolas.ruler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
