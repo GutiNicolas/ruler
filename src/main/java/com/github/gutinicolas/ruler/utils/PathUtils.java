d2dd537b92cc52819e628a0528356074bd6112ca
GutiNicolas <luis.gutierrez@despegar.com>
2020-06-07 00:08:28 -0300

    New function

--- src/main/java/com/github/gutinicolas/ruler/utils/PathUtils.java
+++ src/main/java/com/github/gutinicolas/ruler/utils/PathUtils.java
@@ -1,4 +1,100 @@
 package com.github.gutinicolas.ruler.utils;
 
+import java.io.Serializable;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PathUtils {
+
+    private final static Pattern INDEX = Pattern.compile("^\\\\[(\\\\d+)\\\\]$");
+
+    public static PathObject path(Map<String, Object> container, String fullPath) {
+        String[] paths = fullPath.split("\\.");
+        PathObject result = new PathObject(Optional.of(container));
+        try {
+            for (String path : paths) {
+                result.v = Optional.of(extractValue(result.v, path));
+            }
+            return result;
+        } catch (IllegalArgumentException e) {
+            result.v = null;
+            return result;
+        }
+    }
+
+    private static Object extractValue(Optional<Object> optContainer, String index) {
+        if (!optContainer.isPresent() || optContainer.get() == null) {
+            throw new IllegalArgumentException();
+        }
+        Object container = optContainer.get();
+        Matcher m = INDEX.matcher(index);
+        if (m.matches()) {
+            int i = Integer.parseInt(m.group(1));
+            if (container instanceof Object[]) {
+                return ((Object[]) container)[i];
+            }
+            if (container instanceof List<?>) {
+                return ((List<?>) container).get(i);
+            }
+            throw new IllegalArgumentException("An array or a list was expected, none of them was provided.");
+        }
+        if (container instanceof Map<?, ?>) {
+            return ((Map<?, ?>) container).get(index);
+        }
+        throw new IllegalArgumentException();
+    }
+}
+
+class PathObject implements Serializable {
+    Optional<Object> v;
+
+    public PathObject(Optional<Object> o) {
+        this.v = o;
+    }
+
+    public <T> Optional<T> convert(Class<T> c) {
+        return this.v.isPresent() && c.isInstance(this.v.get()) ? (Optional<T>) this.v : Optional.empty();
+    }
+
+    public Optional<String> toStr() {
+        return convert(String.class);
+    }
+
+    public Optional<Integer> toInteger() {
+        return convert(Integer.class);
+    }
+
+    public Optional<Double> toDouble() {
+        return convert(Double.class);
+    }
+
+    public Optional<Float> toFloat() {
+        return convert(Float.class);
+    }
+
+    public Optional<Boolean> toBoolean() {
+        return convert(Boolean.class);
+    }
+
+    public Optional<UUID> toUUID() {
+        try{
+            return Optional.of(UUID.fromString(this.toStr().orElse("")));
+        } catch (IllegalArgumentException e) {
+            return Optional.empty();
+        }
+    }
+
+    public Map<String, Object> toMap() {
+        return convert(Map.class).orElse(Map.of());
+    }
+
+    public <T> List<T> toList(Class<T> c) {
+        List conv = convert(List.class).orElse(List.of());
+        return conv.isEmpty() ? conv : c.isInstance(conv.get(0)) ? conv : List.of();
+    }
+
+    public <T> Set<T> toSet(Class<T> c) {
+        return new HashSet<T>(this.toList(c));
+    }
 }
